93a93f6eb4cee59256b09b58f345586ddf4fab5a
goodwin-ko <juntkddyd@gmail.com>
2026-04-12 13:29:51 +0900

    Add files via upload

---
+++ data/basicInfo.js
@@ -0,0 +1,6 @@
+const basicInfo = {
+  id: 'goodwin',
+  lastSaved: '2026/04/12 12:08:26'
+};
+
+export default basicInfo;
---
+++ data/equipment.js
@@ -0,0 +1,20 @@
+const equipment = {
+  hero: [
+    { name: '올인원 [NO, 4]', rarity: '강화' },
+    { name: '강화의 룬 [52]', rarity: '강화' },
+    { name: '날만강[HP:1]', rarity: '스탯' },
+    { name: 'Empty', rarity: '' },
+    { name: 'Empty', rarity: '' },
+    { name: 'Empty', rarity: '' }
+  ],
+  bag: [
+    { name: '올인원 [NO, 5]', rarity: '강화' },
+    { name: '히든보스 38 심장', rarity: '심장' },
+    { name: '히든보스 38 악세', rarity: '악세사리' },
+    { name: '히든보스 38 무기', rarity: '무기' },
+    { name: 'Empty', rarity: '' },
+    { name: 'Empty', rarity: '' }
+  ]
+};
+
+export default equipment;
