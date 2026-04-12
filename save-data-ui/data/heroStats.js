fd17d8e9034c6390da81f28ac96d96c47e1f1068
goodwin-ko <juntkddyd@gmail.com>
2026-04-12 13:11:47 +0900

    Add files via upload

---
+++ save-data-ui/data/heroStats.js
@@ -0,0 +1,18 @@
+const heroStats = {
+  str: 500000,
+  agi: 500035,
+  int: 500000,
+  details: {
+    damage: 1000,
+    training: 500,
+    essence: 800,
+    bank: 300,
+    mind: 178708238,
+    energy: 3,
+    vip: 70,
+    rank: 45,
+    rankValue: 45
+  }
+};
+
+export default heroStats;
---
+++ save-data-ui/data/petInfo.js
@@ -0,0 +1,7 @@
+const petInfo = {
+  str: 0,
+  agi: 500010,
+  int: 500000
+};
+
+export default petInfo;
